c66506232a8217dce6d6fead4b4dd2fca2de32e9
slawinski <piotr.slawinski1@gmail.com>
2018-07-09 13:21:03 +0200

    Finishing touches

--- queries.js
+++ queries.js
@@ -7,6 +7,7 @@ const options = {
 };
 
 const pgp = require('pg-promise')(options);
+// const connectionString = 'postgres://postgres:password@localhost:5432/movies_db';
 const connectionString = 'postgres://ccymoxvufmicpo:701ae9fa7a8a08aeaf82ae0556e25be01343bec80bf61acdf139979c844cc68b@ec2-54-227-243-210.compute-1.amazonaws.com:5432/d9bsd10dnsd0aj';
 const db = pgp(connectionString);
 
@@ -20,11 +21,13 @@ module.exports = {
 };
 
 function getAllMovies(req, res, next) {
+  console.log('>>>>> I\'m about to get a list of all movies from the database...')
   db.any('select * from movies')
       .then(function (data) {
         res.render('movies', {
           data: data,
         });
+        console.log('>>>>> Done!')
       })
       .catch(function (err) {
         return next(err);
@@ -41,9 +44,14 @@ function createMovie(req, res, next) {
         return response.json();
       })
       .then(function (myJson) {
-        console.log('>>>>> Got it! Inserting into the database...');
-        db.none('insert into movies(title, genre, year, director, country, actors)' +
-            'values($1, $2, $3, $4, $5, $6)', [myJson.Title, myJson.Genre, myJson.Year, myJson.Director, myJson.Country, myJson.Actors]);
+        if (myJson.Response !== "False") {
+          console.log('>>>>> Got it! Inserting into the database...');
+          db.none('insert into movies(title, genre, year, director, country, actors)' +
+              'values($1, $2, $3, $4, $5, $6)', [myJson.Title, myJson.Genre, myJson.Year, myJson.Director, myJson.Country, myJson.Actors]);
+          console.log('>>>>> Done!');
+        } else {
+          console.log('>>>>> Can\'t find it!');
+        }
       })
       .then(function () {
         res.redirect('/');
@@ -54,11 +62,13 @@ function createMovie(req, res, next) {
 }
 
 function getAllComments(req, res, next) {
+  console.log('>>>>> I\'m about to get a list of all comments from the database...')
   db.any('select * from comments')
       .then(function (data) {
         res.render('comments', {
           data: data,
         });
+        console.log('>>>>> Done!')
       })
       .catch(function (err) {
         return next(err);
@@ -66,12 +76,13 @@ function getAllComments(req, res, next) {
 }
 
 function createComment(req, res, next) {
-  console.log('>>>>> About to insert comment into the database');
+  console.log('>>>>> About to insert comment into the database...');
   console.log('>>>>> Here it is:', req.body);
-  db.none('insert into comments(movie_id, comment)'  +
+  db.none('insert into comments(movie_id, comment)' +
       'values(${movie_id}, ${comment})', req.body)
       .then(function () {
         res.redirect('/');
+        console.log('>>>>> Done!')
       })
       .catch(function (err) {
         return next(err);

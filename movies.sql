d4a9371050800f5fd6ba7c3b05ca8511433358be
slawinski <piotr.slawinski1@gmail.com>
2018-07-09 09:58:30 +0200

    Modify to run heroku successfully

--- movies.sql
+++ movies.sql
@@ -1,7 +1,7 @@
 DROP DATABASE IF EXISTS movies_db;
 CREATE DATABASE movies_db;
 
-\c movies_db;
+
 
 CREATE TABLE movies (
   ID SERIAL PRIMARY KEY,
@@ -20,6 +20,3 @@ CREATE TABLE comments (
   published_date timestamp DEFAULT CURRENT_TIMESTAMP,
   FOREIGN KEY (movie_id) REFERENCES movies(ID) ON DELETE CASCADE
 );
-
---INSERT INTO movies (Title, Year, Director, Country)
---  VALUES ('Ja-nusz w wodzie', 2018, 'Andrzej Pajda', 'Bolzga');

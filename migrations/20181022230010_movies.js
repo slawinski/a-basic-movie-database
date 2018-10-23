d1422f59ec2fe31ce712863d02414ab23370e07a
piotr-s <piotr.slawinski1@gmail.com>
2018-10-23 19:19:14 +0200

    Run migrations

--- migrations/20181022230010_movies.js
+++ migrations/20181022230010_movies.js
@@ -7,18 +7,11 @@ exports.up = function(knex, Promise) {
     Director VARCHAR,
     Country VARCHAR,
     Actors VARCHAR
-  );
-  CREATE TABLE comments (
-    ID SERIAL PRIMARY KEY,
-    movie_id INTEGER,
-    comment VARCHAR,
-    published_date timestamp DEFAULT CURRENT_TIMESTAMP,
-    FOREIGN KEY (movie_id) REFERENCES movies(ID) ON DELETE CASCADE
-  );`;
+  )`;
   return knex.raw(createQuery);
 };
 
 exports.down = function(knex, Promise) {
-  const dropQuery = `DROP DATABASE IF EXISTS movies_db`;
+  const dropQuery = `DROP TABLE movies`;
   return knex.raw(dropQuery);
 };
---
+++ migrations/20181023191042_add_comments_table.js
@@ -0,0 +1,15 @@
+exports.up = function(knex, Promise) {
+  const createQuery = `CREATE TABLE comments (
+    ID SERIAL PRIMARY KEY,
+    movie_id INTEGER,
+    comment VARCHAR,
+    published_date timestamp DEFAULT CURRENT_TIMESTAMP,
+    FOREIGN KEY (movie_id) REFERENCES movies(ID) ON DELETE CASCADE
+  );`;
+  return knex.raw(createQuery);
+};
+
+exports.down = function(knex, Promise) {
+  const dropQuery = `DROP TABLE comments`;
+  return knex.raw(dropQuery);
+};

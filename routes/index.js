063c77e082db07f69721055fa7b55038583f67d5
piotr-s <piotr.slawinski1@gmail.com>
2018-10-31 23:49:28 +0100

    Remove buttons and clean-up

--- routes/index.js
+++ routes/index.js
@@ -4,12 +4,29 @@ const router = express.Router();
 
 const knex = require("../db/knex");
 
+router.get("/", (req, res, next) => {
+  res.render("index");
+});
+
 router.get("/movies", (req, res, next) => {
   knex("movies")
     .select()
     .then(data => {
-      res.render("movies", data);
+      res.render("movies", { data });
     });
+});
+
+router.post("/movies", (req, res, next) => {
+  const url = `http://www.omdbapi.com/?apikey=869369bc&t=${req.body.title}`;
+  fetch(url, {
+    method: "GET"
+  })
+    .then(response => response.json())
+    .then(myJson => {})
+    .then(() => {
+      res.redirect("/");
+    })
+    .catch(err => next(err));
 });
 // router.post("/movies", db.createMovie);
 // router.get("/comments", db.getAllComments);
--- seeds/movie.js
+++ seeds/movie.js
@@ -12,6 +12,15 @@ exports.seed = (knex, Promise) =>
           director: "test-director",
           country: "test-country",
           actors: "test-actors"
+        },
+        {
+          id: "2",
+          title: "test-title2",
+          genre: "test-genre2",
+          year: "2002",
+          director: "test-director2",
+          country: "test-country2",
+          actors: "test-actors2"
         }
       ];
       return knex("movies").insert(movies);
